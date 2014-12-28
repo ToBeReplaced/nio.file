841d50d1d0207757539db2493b88e02237373f8e
ToBeReplaced <ToBeReplaced@gmail.com>
2014-12-28 11:08:04 -0700

    Version 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.tobereplaced/nio.file "0.4.0-SNAPSHOT"
+(defproject org.tobereplaced/nio.file "0.3.1"
   :description "Wrapper for java.nio.file"
   :url "https://github.com/ToBeReplaced/nio.file"
   :license {:name "Eclipse Public License"
